9f4dedb3d8e0960d045b62f6a1629d0dfe258861
최현정 <owo339@naver.com>
2022-01-20 13:13:08 +0900

    chore: MainActivity 패키지 features로 이동

--- mobile/app/src/main/java/com/ssafy/gumi107/mobile_app/features/MainActivity.kt
+++ mobile/app/src/main/java/com/ssafy/gumi107/mobile_app/features/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ssafy.gumi107.mobile_app
+package com.ssafy.gumi107.mobile_app.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ssafy.gumi107.mobile_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
