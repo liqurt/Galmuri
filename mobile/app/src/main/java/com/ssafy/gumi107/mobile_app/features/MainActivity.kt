3ebb1e983eabcf8f4b9943eb75ef8ddde4d21b90
최현정 <owo339@naver.com>
2022-01-24 14:53:39 +0900

    feat: Navigation graph 생성

--- mobile/app/src/main/java/com/ssafy/gumi107/mobile_app/features/MainActivity.kt
+++ mobile/app/src/main/java/com/ssafy/gumi107/mobile_app/features/MainActivity.kt
@@ -3,10 +3,15 @@ package com.ssafy.gumi107.mobile_app.features
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ssafy.gumi107.mobile_app.R
+import com.ssafy.gumi107.mobile_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
