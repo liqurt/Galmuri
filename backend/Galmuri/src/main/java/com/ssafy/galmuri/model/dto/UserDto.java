7c99997d1a35e5e25a9de0fc42c9e1341f488d42
최대호 <cdh6006@naver.com>
2022-01-25 15:14:49 +0900

    feat: User CRUD Controller 구현

--- backend/Galmuri/src/main/java/com/ssafy/galmuri/model/dto/UserDto.java
+++ backend/Galmuri/src/main/java/com/ssafy/galmuri/model/dto/UserDto.java
@@ -6,12 +6,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Builder
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class UserDto {
 	@ApiModelProperty(value = "회원 아이디", required = true, example = "cdh6006@gmail.com")
 	private String userId;
