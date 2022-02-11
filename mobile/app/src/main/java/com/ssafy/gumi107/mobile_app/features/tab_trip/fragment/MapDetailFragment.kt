95d3d32fa20279379f3ab72e67c407fab242b215
윤승일 <liqurtjob@gmail.com>
2022-02-11 09:14:47 +0900

    map 에서 longClickEvent 수신 양호

--- mobile/app/src/main/java/com/ssafy/gumi107/mobile_app/features/tab_trip/fragment/MapDetailFragment.kt
+++ mobile/app/src/main/java/com/ssafy/gumi107/mobile_app/features/tab_trip/fragment/MapDetailFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.ssafy.gumi107.mobile_app.databinding.FragmentMapDetailBinding
 
-class MapDetailFragment : OnMapReadyCallback, Fragment() {
+class MapDetailFragment :
+    GoogleMap.OnMapLongClickListener,OnMapReadyCallback, Fragment() {
 
     private var googleMap : MapView? = null
 
@@ -47,5 +49,12 @@ class MapDetailFragment : OnMapReadyCallback, Fragment() {
         val moonHyungRi = LatLng(37.345095458869, 127.21141094644)
         gm.moveCamera(CameraUpdateFactory.newLatLng(moonHyungRi))
         gm.animateCamera(CameraUpdateFactory.zoomTo(13f))
+
+        gm.setOnMapLongClickListener(this)
+
+    }
+
+    override fun onMapLongClick(p0: LatLng) {
+        Toast.makeText(requireContext(), "$p0", Toast.LENGTH_SHORT).show()
     }
 }
